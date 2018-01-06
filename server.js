f757fe3fcae1b9aaa9fc510b7cca9146e1b0217f
Chris Lesesne <clesesne3@gmail.com>
2018-01-06 10:48:03 -0800

    file creation, server,js, and heroku app create

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+const express = require('express');
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const __API_URL__; /* http://localhost:3000 OR https://git.heroku.com/cl-aa-booklist.git */
+
+const app = express();
+// const conString;
+// const client = new pg.Client();
+// client.connect();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./'));
+
+//app.use(express.static(''));
+
+app.get('/test', (request, response) => {
+  response.send(`Route successful`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
