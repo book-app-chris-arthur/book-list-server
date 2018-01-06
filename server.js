c91437f8681aeae5668d842b7f5d7613a863be9a
Chris Lesesne <clesesne3@gmail.com>
2018-01-06 11:59:10 -0800

    heroku deploy debugging

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const express = require('express');
 const pg = require('pg');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-//const __API_URL__; /* http://localhost:3000 OR https://git.heroku.com/cl-aa-booklist.git */
+//const __API_URL__ = http://localhost:3000
+//const__API_URL__ = https://git.heroku.com/cl-aa-booklist.git
 
 const app = express();
 // const conString;
 // const client = new pg.Client();
 // client.connect();
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('./public'));
-
-//app.use(express.static(''));
 
 app.get('/test', (request, response) => {
   response.send(`Route successful`);
