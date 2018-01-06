3f761791414f915780040e7fa1c73b2042b36ed3
Chris Lesesne <clesesne3@gmail.com>
2018-01-06 14:52:15 -0800

    connecting to DB through Heroku

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ const pg = require('pg');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 //const __API_URL__ = http://localhost:3000
-//const__API_URL__ = https://git.heroku.com/cl-aa-booklist.git
+//const __API_URL__ = https://git.heroku.com/cl-aa-booklist.git
 
 const app = express();
-// const conString;
-// const client = new pg.Client();
-// client.connect();
+const conString = 'postgres://localhost:5432/books_app';
+const dataBaseUrl = process.env.DATABASE_URL;
+
+const client = new pg.Client(dataBaseUrl);
+client.connect();
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/test', (request, response) => {
   response.send(`Route successful`);
+});
+
+app.get('/books', (request, response) => {
+  client.query(`
+    SELECT * FROM books;`
+  )
+    .then(result => response.send(result.rows))
+    .catch(console.error);
 });
 
 app.listen(PORT, () => {
