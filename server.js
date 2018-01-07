678fd0c26e0775b9620b89a34ad894e563ed2c51
Chris Lesesne <clesesne3@gmail.com>
2018-01-06 16:39:51 -0800

    heroku troubleshoot

--- server.js
+++ server.js
@@ -2,31 +2,31 @@
 
 const express = require('express');
 const pg = require('pg');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-//const __API_URL__ = http://localhost:3000
-//const __API_URL__ = https://git.heroku.com/cl-aa-booklist.git
 
 const app = express();
-const conString = 'postgres://localhost:5432/books_app';
-const dataBaseUrl = process.env.DATABASE_URL;
+//const conString = 'postgres://localhost:5432/books_app';
+const DATABASE_URL = process.env.DATABASE_URL;
 
-const client = new pg.Client(dataBaseUrl);
+const client = new pg.Client(DATABASE_URL);
 client.connect();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 app.get('/', (request, response) => {
-  response.send(`Welcom to Chris and Arthur's BookList App`);
+  response.send(`Welcome to Chris and Arthur's BookList App`);
 });
 
 app.get('/test', (request, response) => {
   response.send(`Route successful`);
 });
 
-app.get('/books', (request, response) => {
+app.get('/api/v1/books', (request, response) => {
   client.query(`
-    SELECT * FROM books;`
+    SELECT book_id, title, author, image_url FROM books;`
   )
     .then(result => response.send(result.rows))
     .catch(console.error);
